c92ebb58a36468286ecc405c1acc3966d75d4364
Dynart <Dynart.95@gmail.com>
2021-12-22 03:25:47 -0600

    FrontEnd

---
+++ SistemaTaller.FrontEnd/Dtos/ClientesDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaTaller.FrontEnd.Dtos
+{
+    public class ClientesDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(15, ErrorMessage = "{0} tiene que tener maximo {1} caracteres")]
+        public string CedulaCliente { get; set; }
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(40, ErrorMessage = "{0} Tiene que tener maximo {1} caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(50, ErrorMessage = "{0} Tiene que tener maximo {1} caracteres")]
+        public string Apellidos { get; set; }
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} Tiene que tener maximo {1} caracteres")]
+        public string Telefono { get; set; }
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(50, ErrorMessage = "{0} Tiene que tener maximo {1} caracteres")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(255, ErrorMessage = "{0} Tiene que tener maximo {1} caracteres")]
+        public string Direccion { get; set; }
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(9, ErrorMessage = "{0} Tiene que tener maximo {1} caracteres")]
+        public string VehiculoMatricula { get; set; }
+
+        public bool Activo { get; set; }
+
+    }
+}
---
+++ SistemaTaller.FrontEnd/Dtos/VehiculosDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaTaller.FrontEnd.Dtos
+{
+    public class VehiculosDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene mque tener maximo {1} caracteres")]
+
+        public string Matricula { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene mque tener maximo {1} caracteres")]
+
+        public string MarcaVehiculo { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene mque tener maximo {1} caracteres")]
+
+        public string Modelo { get; set; }
+
+        public bool Activo { get; set; }
+
+    }
+}
